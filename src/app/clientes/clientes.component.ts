83ef91dace80d07867c43a864b6ea99e0a8cab4d
frank <francisco.graya26@gmail.com>
2023-02-08 15:05:49 -0600

    se creo un service de clientes y el uso de inyecion de dependencias en angular

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
 import { CLIENTES } from './clientes.json';
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
 })
-export class ClientesComponent {
+export class ClientesComponent implements OnInit {
 
   clientes: Cliente[];
-  constructor() {}
-  ngOnInit() {
 
-    this.clientes = CLIENTES;
+  constructor(private clienteService: ClienteService) {}
+
+  ngOnInit() {
+    this.clientes = this.clienteService.getClientes();
     console.log('En este instante el componente ha cargado');
   }
 }
